99c6cf8e5668067f2d5fbf21e7cc869fec40eb90
Sára Juhošová <s.juhosova@student.tudelft.nl>
2022-03-15 17:42:19 +0100

    Add output file to prototype arguments

--- prototype.sh
+++ prototype.sh
@@ -12,11 +12,12 @@ log() {
 
 print_help() {
     # Display Help
-    echo "syntax: prototype [-h] [-l] [-s seconds_to_run | -c command_to_run]"
+    echo "syntax: prototype [-h] [-l] [-s seconds_to_run | -c command_to_run] [-o output_file_name]"
     echo "options:"
     echo "c     Run for specified command."
     echo "h     Print this Help."
     echo "l     Run in logging mode."
+    echo "o     Set output file."
     echo "s     Run for specified amount of seconds."
     echo
 }
@@ -26,9 +27,9 @@ print_help() {
 ############################################################
 
 set_mode() {
-    log echo "Attempting to set mode to: "$1
     # if the mode variable is already set
     if [[ -v $MODE ]]; then
+        log echo "Attempting to set mode to: "$1
         # exit with an error
         echo -n "You can only use one mode. "
         echo    "Use either the -s or the -c tag."
@@ -38,33 +39,40 @@ set_mode() {
         MODE=$1
         log echo "Mode is set to: "$MODE
     fi
-    log echo
 }
 
 ############################################################
 # Set the default options.                                 #
 ############################################################
 
+DT=`date | tr -d ' :'`
+
 SECS=60
+OUTPUT=output_$DT.csv
 _l=0
 
 ############################################################
 # Process the input options.                               #
 ############################################################
 
-while getopts ":c:hls:" option; do
+while getopts ":c:hlo:s:" option; do
     case $option in
         c) # set to running for a command
             set_mode "CMD"
-            CMD=$OPTARG;;
+            CMD=$OPTARG
+            log echo "Command set to "$CMD".";;
         h) # display Help
             print_help
             exit;;
         l) # set to verbose
             _l=1;;
+        o) # set output file
+            OUTPUT=$OPTARG
+            log echo "Output file set to "$OUTPUT".";;
         s) # set amount of seconds to run
             set_mode "TIMED"
-            SECS=$OPTARG;;
+            SECS=$OPTARG
+            log echo "Seconds set to "$SECS".";;
         :) # missing argument
             echo "Error: Missing argument"
             exit;;
@@ -76,7 +84,10 @@ done
 
 if [[ -z $MODE ]]; then
     MODE="TIMED"
+    log echo "Mode is set to: "$MODE
 fi
+
+log echo
 
 ############################################################
 # Main programme.                                          #
@@ -87,5 +98,7 @@ if [[ $MODE = "TIMED" ]]; then
 else
     echo "Running for "$CMD" command."
 fi
+
+echo "Results exported to: "$OUTPUT
 
 
