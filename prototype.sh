cef582d4251eaa24f66099c42651e9aa18d97f34
Sára Juhošová <s.juhosova@student.tudelft.nl>
2022-03-16 16:04:17 +0100

    Add output folder flag and fix mode setting

--- prototype.sh
+++ prototype.sh
@@ -12,13 +12,14 @@ log() {
 
 print_help() {
     # Display Help
-    echo "syntax: prototype [-h] [-l] [-s seconds_to_run | -c command_to_run] [-e] [-p] [-t] [-o output_file_name]"
+    echo "syntax: prototype [-h] [-l] [-s seconds_to_run | -c command_to_run] [-e] [-p] [-t] [-o output_file_name | -f output_file_folder]"
     echo "options:"
-    echo "c     Run for specified command."
+    echo "c     Run for specified command. Put the entire command in quotation marks if it is longer than one word."
     echo "e     Print total energy consumption."
+    echo "f     Set the folder in which to save the output file. If it does not exist, it will be created."
     echo "h     Print this Help."
     echo "l     Run in logging mode."
-    echo "o     Set output file."
+    echo "o     Set output file. The path to the file has to exist."
     echo "p     Print average power."
     echo "s     Run for specified amount of seconds."
     echo "t     Print total execution time."
@@ -34,9 +35,9 @@ export() {
 ############################################################
 
 set_mode() {
+    log echo "Attempting to set mode to: "$1
     # if the mode variable is already set
-    if [[ -v $MODE ]]; then
-        log echo "Attempting to set mode to: "$1
+    if [[ -v MODE ]]; then
         # exit with an error
         echo -n "You can only use one mode. "
         echo    "Use either the -s or the -c tag."
@@ -69,28 +70,30 @@ _t=0
 # Process the input options.                               #
 ############################################################
 
-while getopts ":c:ehlo:ps:t" option; do
+while getopts ":c:ef:hlo:ps:t" option; do
     case $option in
         c) # set to running for a command
             set_mode "CMD"
             CMD=$OPTARG
-            log echo "Command set to "$CMD".";;
+            log echo "Command set to: "$CMD;;
         e) # set energy flag
             _e=1;;
+        f) # set output folder
+            FLDR=$OPTARG
+            log echo "Folder set to: "$FLDR;;
         h) # display Help
             print_help
             exit;;
         l) # set to verbose
             _l=1;;
         o) # set output file
-            OUTPUT=$OPTARG
-            log echo "Output file set to "$OUTPUT".";;
+            OUTPUT=$OPTARG;;
         p) # set power flag
             _p=1;;
         s) # set amount of seconds to run
             set_mode "TIMED"
             SECS=$OPTARG
-            log echo "Seconds set to "$SECS".";;
+            log echo "Runtime set to: "$SECS" s";;
         t) # set time flag
             _t=1;;
         :) # missing argument
@@ -102,10 +105,19 @@ while getopts ":c:ehlo:ps:t" option; do
     esac
 done
 
-if [[ -z $MODE ]]; then
+if [[ -z MODE ]]; then
     MODE="TIMED"
     log echo "Mode is set to: "$MODE
 fi
+
+if [[ -v FLDR ]]; then
+    OUTPUT=$FLDR"/"$OUTPUT
+    if [ ! -d $FLDR ]; then
+        mkdir $FLDR
+        log echo "Created folder: "$FLDR
+    fi
+fi
+log echo "Output file set to: "$OUTPUT
 
 log echo
 
